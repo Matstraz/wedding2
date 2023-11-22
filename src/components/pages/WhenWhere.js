efec630db44df5e7b880a69ca6ef7ffdc5a38714
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-11-22 22:51:53 +0100

    feat: add icons to when/where

--- src/components/pages/WhenWhere.js
+++ src/components/pages/WhenWhere.js
@@ -1,5 +1,9 @@
 import church from "../../assets/pics/backgrounds/bg-church.jpg";
 import castle from "../../assets/pics/backgrounds/bg-castle.jpg";
+import info from "../../assets/pics/wedding icons/info.png";
+import map from "../../assets/pics/wedding icons/map.png";
+import birds from "../../assets/pics/wedding icons/birds.png";
+import party from "../../assets/pics/wedding icons/party.png";
 
 export default function WhenWhere() {
   return (
@@ -8,8 +12,9 @@ export default function WhenWhere() {
       <div className="flex justify-center pt-12">
         <div className="flex gap-7 w-full justify-center flex-col lg:flex-row items-center">
           <div className="lg:w-5/12 md:w-10/12 w-11/12">
-            <img src={church} alt="church" className="w-full"></img>
-            <div className="flex flex-col justify-center text-center items-center gap-6 bg-white p-9">
+            <img src={church} alt="church" className="w-full" />
+            <div className="flex flex-col justify-center text-center items-center gap-6 bg-white p-9 relative">
+              <img src={birds} alt="birds" className="absolute -top-5 w-20" />
               <p className="italic text-slate-700">LA CERIMONIA</p>
               <p>
                 La chiesa di Santa Maria della Pietà è un luogo di culto
@@ -23,26 +28,21 @@ export default function WhenWhere() {
                 Venerdì 5/07/2024 - 15.30 PM - Chiesa di Santa Maria della
                 Pietà, Piazzetta della Pietà SNC, 90133 Palermo
               </p>
-              <div className="flex gap-5">
-                <a
-                  className="bg-myBlue-bgDark p-1 px-4"
-                  href="https://it.wikipedia.org/wiki/Chiesa_di_Santa_Maria_della_Piet%C3%A0_(Palermo)"
-                >
-                  Info
+              <div className="flex gap-20 md:gap-28">
+                <a href="https://it.wikipedia.org/wiki/Chiesa_di_Santa_Maria_della_Piet%C3%A0_(Palermo)">
+                  <img src={info} alt="info" className="w-14" />
                 </a>
-                <a
-                  className="bg-myBlue-bgDark p-1 px-4"
-                  href="https://www.google.it/maps/dir//Chiesa+Santa+Maria+Della+Piet%C3%A0,+Piazzetta+della+Piet%C3%A0,+Palermo,+PA/@38.1174972,13.3716383,202m/data=!3m1!1e3!4m9!4m8!1m0!1m5!1m1!1s0x1319e52ede184e8b:0x58413da94c6faeb6!2m2!1d13.3721157!2d38.1169877!3e0"
-                >
-                  Indicazioni stradali
+                <a href="https://www.google.it/maps/dir//Chiesa+Santa+Maria+Della+Piet%C3%A0,+Piazzetta+della+Piet%C3%A0,+Palermo,+PA/@38.1174972,13.3716383,202m/data=!3m1!1e3!4m9!4m8!1m0!1m5!1m1!1s0x1319e52ede184e8b:0x58413da94c6faeb6!2m2!1d13.3721157!2d38.1169877!3e0">
+                  <img src={map} alt="map" className="w-7" />
                 </a>
               </div>
             </div>
           </div>
           <div className="lg:w-5/12 md:w-10/12 w-11/12">
-            <img src={castle} alt="castle" className="w-full"></img>
+            <img src={castle} alt="castle" className="w-full" />
+            <div className="flex flex-col justify-center items-center text-center gap-6 bg-white p-9 relative">
+              <img src={party} alt="birds" className="absolute -top-11 w-11" />
 
-            <div className="flex flex-col justify-center items-center text-center gap-6 bg-white p-9">
               <p className="italic text-slate-700">IL RICEVIMENTO</p>
               <p>
                 Il Castello di San Nicola L’Arena fu edificato nel XIV secolo,
@@ -56,18 +56,12 @@ export default function WhenWhere() {
                 Venerdì 5/07/2024 - 15.30 PM - Chiesa di Santa Maria della
                 Pietà, Piazzetta della Pietà SNC, 90133 Palermo
               </p>
-              <div className="flex gap-5">
-                <a
-                  className="bg-myBlue-bgDark p-1 px-4"
-                  href="http://www.castellodisannicola.it/"
-                >
-                  Info
+              <div className="flex gap-20 md:gap-28">
+                <a href="http://www.castellodisannicola.it/">
+                  <img src={info} alt="info" className="w-14" />
                 </a>
-                <a
-                  className="bg-myBlue-bgDark p-1 px-4"
-                  href="https://www.google.it/maps/dir//Castello+San+Nicola,+l'Arena,+San+Nicola+l'Arena+PA/@38.0155572,13.6137298,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x1317575d277f63b1:0xe4f4db2eebe2264e!2m2!1d13.6158976!2d38.0155676!3e0"
-                >
-                  Indicazioni stradali
+                <a href="https://www.google.it/maps/dir//Castello+San+Nicola,+l'Arena,+San+Nicola+l'Arena+PA/@38.0155572,13.6137298,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x1317575d277f63b1:0xe4f4db2eebe2264e!2m2!1d13.6158976!2d38.0155676!3e0">
+                  <img src={map} alt="map" className="w-7" />
                 </a>
               </div>
             </div>
--- src/components/utils/Animations.js
+++ src/components/utils/Animations.js
@@ -1,15 +1,11 @@
 import { StyleSheet } from "aphrodite";
-import { fadeInRight, slideInDown, tada, pulse } from "react-animations";
+import { slideInDown, tada, pulse } from "react-animations";
 
 const styles = StyleSheet.create({
   infiniteBounce: {
     animationName: slideInDown,
     animationDuration: "2.5s",
     animationIterationCount: "infinite",
-  },
-  fadeInRight: {
-    animationName: fadeInRight,
-    animationDuration: "1s",
   },
   tada: {
     animationName: tada,
