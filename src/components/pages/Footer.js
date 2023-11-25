41ac68cd1e8b5e729455d2bb8d6d8bf689a6022e
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-11-25 19:53:48 +0100

    feat: create Iban component + fix footre + add favicon

--- src/components/pages/Footer.js
+++ src/components/pages/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="bg-myBlue-bgDark text-center py-20">
-      Frase biblica.. + sfondo
+    <div className="bg-myBlue-bgDark p-2 flex justify-start items-center gap-4">
+      © 2024 <a href="/">Alessandra&Matteo™</a>
     </div>
   );
 }
