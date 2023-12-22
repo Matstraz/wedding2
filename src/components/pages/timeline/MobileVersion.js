4682130867859c6bd1e366babaa9cb09798b5697
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2023-12-22 12:49:09 +0100

    feat: fix timetable mobile version

--- src/components/pages/timeline/MobileVersion.js
+++ src/components/pages/timeline/MobileVersion.js
@@ -4,7 +4,8 @@ import music from "../../../assets/pics/timeline/music.png";
 import rings from "../../../assets/pics/timeline/rings.png";
 import dinner from "../../../assets/pics/timeline/dinner.png";
 import arrow1 from "../../../assets/pics/timeline/arrows/arrow1.png";
-import arrow2 from "../../../assets/pics/timeline/arrows/arrow2.png";
+/* loop arrow */
+/* import arrow2 from "../../../assets/pics/timeline/arrows/arrow2.png"; */
 import arrow3 from "../../../assets/pics/timeline/arrows/arrow3.png";
 import arrow4 from "../../../assets/pics/timeline/arrows/arrow4.png";
 import arrow5 from "../../../assets/pics/timeline/arrows/arrow5.png";
@@ -31,35 +32,36 @@ export default function DesktopVersion({ animate, setAnimate }) {
         </div>
         <div className="centeredDiv">
           <img src={dinner} alt="dinner"></img>
-          <p className="text-center">12:00</p>
+          <p className="text-center">19:00</p>
         </div>
         <div className="centeredArrows">
           <img className="arrow5" src={arrow5} alt="arrow5"></img>
         </div>
         <div className="centeredDiv">
           <img src={music} alt="music"></img>
-          <p className="text-center">12:00</p>
+          <p className="text-center">22:00</p>
         </div>
         <div className="centeredArrows">
           <img className="arrow1" src={arrow1} alt="arrow1"></img>
         </div>
         <div className="centeredDiv">
           <img src={cake} alt="cake"></img>
-          <p className="text-center">12:00</p>
+          <p className="text-center">24:00</p>
         </div>
         <div className="centeredArrows">
-          <img className="arrow2" src={arrow2} alt="arrow2"></img>
+          {/* loop arrow */}
+          {/*    <img className="arrow2" src={arrow2} alt="arrow2"></img> */}
         </div>
         <div className="centeredDiv">
           <img src={rings} alt="rings"></img>
-          <p className="text-center">12:00</p>
+          <p className="text-center">15:30</p>
         </div>
         <div className="centeredArrows">
           <img className="arrow3" src={arrow3} alt="arrow3"></img>
         </div>
         <div className="centeredDiv">
           <img src={drink} alt="drink"></img>
-          <p className="text-center">12:00</p>
+          <p className="text-center">16:30</p>
         </div>
       </div>
     </div>
